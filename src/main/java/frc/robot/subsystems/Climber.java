016f3f70e80080dd6aafb2b76ed867ef95497419
redninjale <redninjale@gmail.com>
2022-02-21 16:27:38 -0600

    changed climber to double solenoids

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,8 +5,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
@@ -20,8 +22,8 @@ public class Climber extends SubsystemBase {
 
   private final TalonFX leftLiftMotor;
   private final TalonFX rightLiftMotor;
-  private Solenoid leftSolenoid;
-  private Solenoid rightSolenoid;
+  private DoubleSolenoid leftSolenoid;
+  private DoubleSolenoid rightSolenoid;
   // private Encoder liftEncoder;
 
   public Object climbUp;
@@ -29,15 +31,15 @@ public class Climber extends SubsystemBase {
 
   /** Creates a new Climber. */
   public Climber() {
-    //dania did this section
-    //comment 
+    //assigns motors to the falcon controller   
     leftLiftMotor = new TalonFX(ClimberConstants.LEFT_LIFT_MOTOR);
     rightLiftMotor = new TalonFX(ClimberConstants.RIGHT_LIFT_MOTOR);
     leftLiftMotor.setNeutralMode(NeutralMode.Brake);
     rightLiftMotor.setNeutralMode(NeutralMode.Brake);
     // liftMotor = new Encoder
-    leftSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.LEFT_SOLENOID);
-    rightSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.RIGHT_SOLENOID);
+    // leftSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.LEFT_SOLENOID);
+    leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, ClimberConstants.LEFT_FOR_SOLENOID, ClimberConstants.LEFT_REV_SOLENOID);
+    rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, ClimberConstants.RIGHT_FOR_SOLENOID, ClimberConstants.RIGHT_REV_SOLENOID);
   }
 
   public void setClimbMotor(double percent) {
@@ -58,13 +60,18 @@ public class Climber extends SubsystemBase {
   }
 
   public void climbOpenSol() {
-    leftSolenoid.set(true);
-    rightSolenoid.set(true);
+    leftSolenoid.set(Value.kForward);
+    rightSolenoid.set(Value.kForward);
   }
 
   public void climbCloseSol() {
-    leftSolenoid.set(false);
-    rightSolenoid.set(false);
+    leftSolenoid.set(Value.kReverse);
+    rightSolenoid.set(Value.kReverse);
+  }
+ 
+  public void climbToggleSol(){
+    leftSolenoid.toggle();;
+    rightSolenoid.toggle();
   }
 
   public void dashboard() {
